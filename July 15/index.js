a0b8e06db8965a761008556be0430ab965af64ab
Joe <agarg51370@gmail.com>
2025-07-16 11:55:27 +0530

    NodeJs

---
+++ July 15/index.js
@@ -0,0 +1,28 @@
+console.log("ENTRY LEVEL FILE")
+
+
+var xy = 200;
+
+function sumOfTwo(a,b){
+
+    console.log(a+b);
+}
+
+function divOfTwo(a,b){
+    console.log(a/b);
+}
+
+function subOfTwo(a,b){
+
+    console.log(a-b);
+}
+
+function mulOfTwo(a,b){
+    console.log(a*b);
+}
+
+console.log(xy)
+sumOfTwo(200,400);
+subOfTwo(500,200);
+divOfTwo(540,20);
+mulOfTwo(10,300);
